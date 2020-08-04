0d50f46944d72b5da489eb5e76c10cbedb1fef9a
Sergey Lebedev <lebedev_sergey81@mail.ru>
2020-08-04 20:00:01 +0300

    Create strong_number.js

---
+++ strong_number.js
@@ -0,0 +1,12 @@
+// For example: 145, since
+// 1! + 4! + 5! = 1 + 24 + 120 = 145
+// So, 145 is a Strong number.
+
+function strong(n) {
+  const factorial = (b) => {
+    if (b === 0) return 1;
+    return b * factorial(b - 1);
+  }
+  const num = [...`${n}`].map(item => factorial(item)).reduce((a, b) => a + b, 0);
+  return n === num ? "STRONG!!!!" : "Not Strong !!" ;
+}
