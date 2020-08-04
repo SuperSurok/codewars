5d3a59013160721a0e625a4cc6b30cb7583d6bfb
Sergey Lebedev <lebedev_sergey81@mail.ru>
2020-08-04 20:06:30 +0300

    Update strong_number.js

--- strong_number.js
+++ strong_number.js
@@ -4,7 +4,7 @@
 
 function strong(n) {
   const factorial = (b) => {
-    if (b === 0) return 1;
+    if (b <= 0) return 1;
     return b * factorial(b - 1);
   }
   const num = [...`${n}`].map(item => factorial(item)).reduce((a, b) => a + b, 0);
